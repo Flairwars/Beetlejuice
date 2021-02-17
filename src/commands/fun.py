db0772402af8a4f8bf713a7d3e929179d7b7b78e
Wander <keyleasia@gmail.com>
2021-02-16 20:50:18 -0700

    Added Hugs
    Random Hugs however are not working

--- src/commands/fun.py
+++ src/commands/fun.py
@@ -1,3 +1,4 @@
+import discord
 from discord.ext import commands
 import random
 
@@ -8,23 +9,33 @@ class Fun(commands.Cog, name='Fun'):
     def __init__(self, client):
         self.client = client
 
-    @commands.command(aliases=['hug'])
+    @commands.command(aliases=['hugs'])
     async def hug(self, ctx, target: discord.Member = None):
+        hug_urls = ["https://imgur.com/gvC4K6u", "https://imgur.com/9RGBOLa", "https://imgur.com/ZRGpg8P",
+                    "https://imgur.com/7APfwfg", "https://imgur.com/xTJdcbg", "https://imgur.com/D1bJQWL",
+                    "https://imgur.com/3AB9LXN", "https://imgur.com/xwogdA8", "https://imgur.com/3aSRyez",
+                    "https://imgur.com/OMtbHom", "https://imgur.com/XALtUs4", "https://imgur.com/EIfimk8",
+                    "https://imgur.com/BEG77iA", "https://imgur.com/qxeXsRr", "https://imgur.com/osZF2GM",
+                    "https://imgur.com/PqotrGa", "https://imgur.com/sYCVhZ1", "https://imgur.com/ItZk2E7",
+                    "https://imgur.com/ng81k8S", "https://imgur.com/4bGa09O"]
 
         if target is None:
             await ctx.send(f"*hugs back*")
 
         elif target == ctx.author:
-            file = str(5) + '.gif'
-            await ctx.send(file=discord.File('src/data/hugs/' + file))
+            await ctx.send("https://imgur.com/xTJdcbg")
             return
 
         else:
-            file = str(random.randint(1, 26)) + '.gif'
-            await ctx.send(file=discord.File('src/data/hugs/' + file))
+            hug_result = random.shuffle(hug_urls)
+            await ctx.send(f"{hug_result}")
             return
-        await ctx.send('ping')
 
 
 def setup(client):
     client.add_cog(Fun(client))
+
+
+cars = ["Ford", "Volvo", "BMW"]
+
+cars[0] = "Toyota"
