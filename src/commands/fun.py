be1dcd0840c31adca357af170f649c6bb8c50feb
Blotz <f.p.theil@gmail.com>
2021-02-20 22:10:03 +0000

    Added if statements to check whether user has a nick or not

--- src/commands/fun.py
+++ src/commands/fun.py
@@ -4,13 +4,15 @@ import random
 
 class Fun(commands.Cog, name='Fun'):
     """
-    F is for friends and uh U is for I forgot and N is for now? Idk this song
+    Full of lots of fun commands
     """
     def __init__(self, client):
         self.client = client
 
     @commands.command(aliases=['hugs'])
     async def hug(self, ctx, target: discord.Member = None):
+        """Provides hugs!
+        """
         hug_urls = ["https://imgur.com/gvC4K6u", "https://imgur.com/9RGBOLa", "https://imgur.com/ZRGpg8P",
                     "https://imgur.com/7APfwfg", "https://imgur.com/xTJdcbg", "https://imgur.com/D1bJQWL",
                     "https://imgur.com/3AB9LXN", "https://imgur.com/xwogdA8", "https://imgur.com/3aSRyez",
@@ -18,26 +20,19 @@ class Fun(commands.Cog, name='Fun'):
                     "https://imgur.com/BEG77iA", "https://imgur.com/qxeXsRr", "https://imgur.com/osZF2GM",
                     "https://imgur.com/PqotrGa", "https://imgur.com/sYCVhZ1", "https://imgur.com/ItZk2E7",
                     "https://imgur.com/ng81k8S", "https://imgur.com/4bGa09O"]
-        hug_author = ctx.author.nick
 
         if target is None:
             await ctx.send(f"*hugs back*")
-
         elif target == ctx.author:
             await ctx.send("https://imgur.com/xTJdcbg")
-            return
-
         else:
             hug_result = random.choice(hug_urls)
-            await ctx.send(f"A hug from {hug_author}")
+
+            author_name = ctx.author.name if ctx.author.nick is None else ctx.author.nick
+
+            await ctx.send(f"A hug from {author_name}!")
             await ctx.send(f"{hug_result}")
-            return
 
 
 def setup(client):
     client.add_cog(Fun(client))
-
-
-cars = ["Ford", "Volvo", "BMW"]
-
-cars[0] = "Toyota"
