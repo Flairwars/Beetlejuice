9037a0f0c8c76c67bc88ffa861c1c4daad53f5ee
rebalint <rebalint@gmail.com>
2021-04-03 14:16:26 +0200

    fix

--- src/commands/image.py
+++ src/commands/image.py
@@ -155,22 +155,22 @@ class ImageEditing(commands.Cog, name='image'):
         await ctx.send(f'`{color}@{int(strength * 100)}%`', file=f)
         await bot_msg.delete()
 
-"""
-    @recolor.error
-    async def _recolor(self, ctx: object, error: object):
-        """
-        Error output for Recolor
-        :param ctx:
-        :param error:
-        :return:
-        """
-        if isinstance(error, commands.CommandInvokeError):
-            await ctx.send('`ERROR: invalid args <color> <percentage>`')
-        elif isinstance(error, discord.errors.DiscordException):
-            await ctx.send('`ERROR: invalid color`')
-        else:
-            await ctx.send('`ERROR: something went wrong`')
-"""
+
+    # @recolor.error
+    # async def _recolor(self, ctx: object, error: object):
+    #     """
+    #     Error output for Recolor
+    #     :param ctx:
+    #     :param error:
+    #     :return:
+    #     """
+    #     if isinstance(error, commands.CommandInvokeError):
+    #         await ctx.send('`ERROR: invalid args <color> <percentage>`')
+    #     elif isinstance(error, discord.errors.DiscordException):
+    #         await ctx.send('`ERROR: invalid color`')
+    #     else:
+    #         await ctx.send('`ERROR: something went wrong`')
+
     @staticmethod
     def ProcessBee(img: Image, beecount: int, width: int, height: int):
         """Adds bee pngs to an image
